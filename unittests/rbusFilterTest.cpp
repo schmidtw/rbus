4c7859c0344843a294647b582f77b8d8c78886e0
Rajkamal CV <Rajkamal_CorneliusSoundararajVimala@comcast.com>
2020-12-28 16:38:46 +0530

    RDKB-32347 : Integrate gtest and code coverage into rbus
    Reason for change: Add gtest for rbus APIs
    Risks: Low
    Test Procedure: Compile and run gtest for RBUS-2.0
    Signed-off-by: Rajkamal CV <Rajkamal_CorneliusSoundararajVimala@comcast.com>
    
    (cherry picked from commit 761a571004f98efedddb45f746c1e4dd36f41b7c)
    Change-Id: I6da1b388e0e96ab07fa524bc84cf3c20275077f9

---
+++ unittests/rbusFilterTest.cpp
@@ -0,0 +1,360 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file
+ * the following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "gtest/gtest.h"
+
+#include <rbus.h>
+
+TEST(rbusFilterInitTest, testFilterInit1)
+{
+  rbusFilter_t filter = NULL;
+  rbusFilter_RelationOperator_t op = RBUS_FILTER_OPERATOR_EQUAL;
+  rbusValue_t val;
+
+  rbusValue_Init(&val);
+  EXPECT_EQ(rbusValue_SetFromString(val,RBUS_STRING,"string"),true);
+  rbusFilter_InitRelation(&filter, op, val);
+
+  EXPECT_EQ(RBUS_FILTER_EXPRESSION_RELATION,rbusFilter_GetType(filter));
+  rbusValue_Release(val);
+  rbusFilter_Release(filter);
+}
+
+TEST(rbusFilterInitTest, testFilterInit2)
+{
+  rbusFilter_t filter = NULL;
+  rbusFilter_LogicOperator_t op = RBUS_FILTER_OPERATOR_AND;
+  rbusFilter_InitLogic(&filter, op, NULL,NULL);
+
+  EXPECT_EQ(RBUS_FILTER_EXPRESSION_LOGIC,rbusFilter_GetType(filter));
+  EXPECT_EQ(nullptr,rbusFilter_GetLogicLeft(filter));
+  EXPECT_EQ(nullptr,rbusFilter_GetLogicRight(filter));
+  rbusFilter_Release(filter);
+}
+
+TEST(rbusFilterInitTest, testFilterInit3)
+{
+  rbusFilter_t filter,filter_left,filter_right;
+  rbusFilter_LogicOperator_t op = RBUS_FILTER_OPERATOR_AND;
+  rbusFilter_InitLogic(&filter_left, op, NULL,NULL);
+  rbusFilter_InitLogic(&filter_right, op, NULL,NULL);
+  rbusFilter_InitLogic(&filter, op, filter_left,filter_right);
+
+  EXPECT_EQ(RBUS_FILTER_EXPRESSION_LOGIC,rbusFilter_GetType(filter));
+  EXPECT_EQ(filter_left,rbusFilter_GetLogicLeft(filter));
+  EXPECT_EQ(filter_right,rbusFilter_GetLogicRight(filter));
+  rbusFilter_Release(filter);
+}
+
+static void execRbusFilterLogicOperatorTest(rbusFilter_LogicOperator_t op)
+{
+  rbusFilter_t filter;
+  rbusFilter_InitLogic(&filter, op, NULL,NULL);
+
+  EXPECT_EQ(RBUS_FILTER_EXPRESSION_LOGIC,rbusFilter_GetType(filter));
+  EXPECT_EQ(op,rbusFilter_GetLogicOperator(filter));
+  rbusFilter_Release(filter);
+}
+
+static void execRbusFilterRelOperatorTest(rbusFilter_RelationOperator_t op)
+{
+  rbusFilter_t filter = NULL;
+  rbusValue_t val, val_cmp;
+
+  rbusValue_Init(&val);
+  EXPECT_EQ(rbusValue_SetFromString(val,RBUS_STRING,"string"),true);
+  rbusFilter_InitRelation(&filter, op, val);
+
+  EXPECT_EQ(RBUS_FILTER_EXPRESSION_RELATION,rbusFilter_GetType(filter));
+  EXPECT_EQ(op,rbusFilter_GetRelationOperator(filter));
+  val_cmp = rbusFilter_GetRelationValue(filter);
+  EXPECT_EQ(rbusValue_Compare( val, val_cmp),0);
+  rbusValue_Release(val);
+  rbusFilter_Release(filter);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator1)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_EQUAL);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator2)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_NOT_EQUAL);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator3)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_LESS_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator4)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_LESS_THAN);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator5)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_GREATER_THAN);
+}
+
+TEST(rbusFilterRelOperatorTest, testFilterRelOperator6)
+{
+  execRbusFilterRelOperatorTest(RBUS_FILTER_OPERATOR_GREATER_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterLogicOperatorTest, testFilterLogicOperator1)
+{
+  execRbusFilterLogicOperatorTest(RBUS_FILTER_OPERATOR_AND);
+}
+
+TEST(rbusFilterLogicOperatorTest, testFilterLogicOperator2)
+{
+  execRbusFilterLogicOperatorTest(RBUS_FILTER_OPERATOR_OR);
+}
+
+TEST(rbusFilterLogicOperatorTest, testFilterLogicOperator3)
+{
+  execRbusFilterLogicOperatorTest(RBUS_FILTER_OPERATOR_NOT);
+}
+
+static void execRbusFilterApplyTest(char *buffer1, char *buffer2, char *filter_buf,rbusFilter_LogicOperator_t op_logic,rbusFilter_RelationOperator_t op_rel)
+{
+  rbusValue_t val1, val2, val3;
+  rbusFilter_t filter_rel1, filter_rel2, filter;
+  bool ret;
+  FILE *stream;
+  char *stream_buf;
+  size_t len;
+
+  stream = open_memstream(&stream_buf, &len);
+
+  rbusValue_Init(&val1);
+  EXPECT_EQ(rbusValue_SetFromString(val1,RBUS_STRING,buffer1),true);
+  rbusFilter_InitRelation(&filter_rel1, op_rel, val1);
+
+  rbusValue_Init(&val2);
+  EXPECT_EQ(rbusValue_SetFromString(val2,RBUS_STRING,((buffer2) ? (buffer2):(""))),true);
+  rbusFilter_InitRelation(&filter_rel2, op_rel, val2);
+
+  rbusValue_Init(&val3);
+  EXPECT_EQ(rbusValue_SetFromString(val3,RBUS_STRING,filter_buf),true);
+
+  rbusFilter_InitLogic(&filter, op_logic, filter_rel1,filter_rel2);
+
+  rbusFilter_fwrite(filter,stream,val3);
+
+  EXPECT_EQ(rbusFilter_Apply(filter,val3),true);
+
+  rbusFilter_Release(filter);
+  rbusValue_Release(val1);
+  rbusValue_Release(val2);
+  rbusValue_Release(val3);
+  fflush(stream);
+  fclose(stream);
+
+  switch(op_logic)
+  {
+    case RBUS_FILTER_OPERATOR_AND:
+      EXPECT_NE(nullptr,strstr(stream_buf,"&&"));
+    break;
+    case RBUS_FILTER_OPERATOR_OR:
+      EXPECT_NE(nullptr,strstr(stream_buf,"||"));
+    break;
+    case RBUS_FILTER_OPERATOR_NOT:
+      EXPECT_NE(nullptr,strstr(stream_buf,"!("));
+    break;
+  }
+  switch(op_rel)
+  {
+    case RBUS_FILTER_OPERATOR_EQUAL:
+      EXPECT_NE(nullptr,strstr(stream_buf,"=="));
+    break;
+    case RBUS_FILTER_OPERATOR_NOT_EQUAL:
+      EXPECT_NE(nullptr,strstr(stream_buf,"!="));
+    break;
+    case RBUS_FILTER_OPERATOR_GREATER_THAN:
+      EXPECT_NE(nullptr,strstr(stream_buf,">"));
+    break;
+    case RBUS_FILTER_OPERATOR_GREATER_THAN_OR_EQUAL:
+      EXPECT_NE(nullptr,strstr(stream_buf,">="));
+    break;
+    case RBUS_FILTER_OPERATOR_LESS_THAN:
+      EXPECT_NE(nullptr,strstr(stream_buf,"<"));
+    break;
+    case RBUS_FILTER_OPERATOR_LESS_THAN_OR_EQUAL:
+      EXPECT_NE(nullptr,strstr(stream_buf,"<="));
+    break;
+  }
+}
+
+TEST(rbusFilterApplyTest, testFilterApply1)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string";
+  char filter_buf[]="test string";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply2)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string";
+  char filter_buf[]="test strin1";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_NOT_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply3)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_GREATER_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply4)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string";
+  char filter_buf[]="test stri";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_LESS_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply5)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string12";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_GREATER_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply6)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test stri";
+  char filter_buf[]="test stri";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_AND, RBUS_FILTER_OPERATOR_LESS_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply7)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test strin1";
+  char filter_buf[]="test string";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply8)
+{
+  char buffer1[]="test striqg";
+  char buffer2[]="test string";
+  char filter_buf[]="test strin1";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_NOT_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply9)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_GREATER_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply10)
+{
+  char buffer1[]="test str";
+  char buffer2[]="test string";
+  char filter_buf[]="test stri";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_LESS_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply11)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test string12";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_GREATER_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply12)
+{
+  char buffer1[]="test string";
+  char buffer2[]="test stri";
+  char filter_buf[]="test stri";
+
+  execRbusFilterApplyTest(buffer1, buffer2, filter_buf, RBUS_FILTER_OPERATOR_OR, RBUS_FILTER_OPERATOR_LESS_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply13)
+{
+  char buffer[]="test string1";
+  char filter_buf[]="test string2";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply14)
+{
+  char buffer[]="test string1";
+  char filter_buf[]="test string1";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_NOT_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply15)
+{
+  char buffer[]="test string12";
+  char filter_buf[]="test string1";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_GREATER_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply16)
+{
+  char buffer[]="test string1";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_LESS_THAN);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply17)
+{
+  char buffer[]="test string12";
+  char filter_buf[]="test string1";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_GREATER_THAN_OR_EQUAL);
+}
+
+TEST(rbusFilterApplyTest, testFilterApply18)
+{
+  char buffer[]="test string1";
+  char filter_buf[]="test string12";
+
+  execRbusFilterApplyTest(buffer, NULL, filter_buf, RBUS_FILTER_OPERATOR_NOT, RBUS_FILTER_OPERATOR_LESS_THAN_OR_EQUAL);
+}
