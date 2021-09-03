c5ae002a988d82fdd87972a55194de91e983a473
Gururaaja E S R <gururaja_erodesriranganramlingham@comcast.com>
2021-09-03 17:25:52 +0530

    RDKB-37296 - Rbus set api is not giving proper error code
    Reason for change: Added rbus_set test case in sample consumer to validate
    Test Procedure: Build and verified
    Risks: Low
    Signed-off-by: Gururaaja E S R <gururaja_erodesriranganramlingham@comcast.com>
    Change-Id: Ibc8c41bfd02000c58dfbd9df6f83363acd96f3e5

--- sampleapps/consumer/rbusSampleConsumer.c
+++ sampleapps/consumer/rbusSampleConsumer.c
@@ -45,6 +45,51 @@ char const*     paramNames[TotalParams] = {
     "Device.SampleProvider.NestedObject2.AnotherTestParam"*/
 };
 
+rbusValueType_t getDataType_fromString(const char* pType)
+{
+    rbusValueType_t rc = RBUS_NONE;
+
+    if (strncasecmp ("boolean", pType, 4) == 0)
+        rc = RBUS_BOOLEAN;
+    else if (strncasecmp("char", pType, 4) == 0)
+        rc = RBUS_CHAR;
+    else if (strncasecmp("byte", pType, 4) == 0)
+        rc = RBUS_BYTE;
+    else if (strncasecmp("int8", pType, 4) == 0)
+        rc = RBUS_INT8;
+    else if (strncasecmp("uint8", pType, 5) == 0)
+        rc = RBUS_UINT8;
+    else if (strncasecmp("int16", pType, 5) == 0)
+        rc = RBUS_INT16;
+    else if (strncasecmp("uint16", pType, 6) == 0)
+        rc = RBUS_UINT16;
+    else if (strncasecmp("int32", pType, 5) == 0)
+        rc = RBUS_INT32;
+    else if (strncasecmp("uint32", pType, 6) == 0)
+        rc = RBUS_UINT32;
+    else if (strncasecmp("int64", pType, 5) == 0)
+        rc = RBUS_INT64;
+    else if (strncasecmp("uint64", pType, 6) == 0)
+        rc = RBUS_UINT64;
+    else if (strncasecmp("single", pType, 5) == 0)
+        rc = RBUS_SINGLE;
+    else if (strncasecmp("double", pType, 6) == 0)
+        rc = RBUS_DOUBLE;
+    else if (strncasecmp("datetime", pType, 4) == 0)
+        rc = RBUS_DATETIME;
+    else if (strncasecmp("string", pType, 6) == 0)
+        rc = RBUS_STRING;
+    else if (strncasecmp("bytes", pType, 3) == 0)
+        rc = RBUS_BYTES;
+    /* Risk handling, if the user types just int, lets consider int32; same for unsigned too  */
+    else if (strncasecmp("int", pType, 3) == 0)
+        rc = RBUS_INT32;
+    else if (strncasecmp("uint", pType, 4) == 0)
+        rc = RBUS_UINT32;
+
+    return rc;
+}
+
 int main(int argc, char *argv[])
 {
     int rc;
@@ -54,6 +99,24 @@ int main(int argc, char *argv[])
     (void)argv;
 
     printf("constumer: start\n");
+
+    if(argv[1] != NULL && argc == 2)
+    {
+        if((strcmp(argv[1], "--help")==0) || (strcmp(argv[1], "-h")==0))
+        {
+            printf("rbusSampleConsumer [OPTIONS] \n");
+            printf("[OPTIONS]");
+            printf("-h/--help: to display help\n");
+            printf("\t\t**********************************************************************\n\
+                *************************  rbus_set testing  *************************\n\
+                **********************************************************************\n\
+                Please pass the arguments for testing in below order\n\
+                rbusSampleConsumer <data model> <data type> <data value>\n\
+                For Ex: rbusSampleConsumer Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.MessageBusSource.Enable boolean true\n\
+                caution: If invalid data model, data type or data value is passed this case is expected to be failed\n");
+            return 0;
+        }
+    }
 
     rc = rbus_open(&handle, componentName);
     if(rc != RBUS_ERROR_SUCCESS)
@@ -118,6 +181,12 @@ int main(int argc, char *argv[])
             rc = rbus_set(handle, paramNames[count], value, &opts);
             if (rc != RBUS_ERROR_SUCCESS)
                 printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
+            /* -ve Test with invalid data type. it will fail because it expects type Int32*/
+            printf("calling rbus set for [%s] with invalid data type.  this should fail.\n", paramNames[count]);
+            rbusValue_SetFromString(value, RBUS_STRING, "500");
+            rc = rbus_set(handle, paramNames[count], value, &opts);
+            if (rc != RBUS_ERROR_SUCCESS)
+                printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
             rbusValue_Release(value);
 
             printf("calling rbus get for [%s]\n", paramNames[count]);
@@ -140,6 +209,12 @@ int main(int argc, char *argv[])
             /* -ve Test. it will fail because it expects type Int32, not String*/
             printf("calling rbus set for [%s] with string value.  this should fail.\n", paramNames[count]);
             rbusValue_SetString(value, "Test");
+            rc = rbus_set(handle, paramNames[count], value, &opts);
+            if (rc != RBUS_ERROR_SUCCESS)
+                printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
+            /* -ve Test with invalid data type. it will fail because it expects type bool*/
+            printf("calling rbus set for [%s] with invalid data type.  this should fail.\n", paramNames[count]);
+            rbusValue_SetFromString(value, RBUS_STRING, "true");
             rc = rbus_set(handle, paramNames[count], value, &opts);
             if (rc != RBUS_ERROR_SUCCESS)
                 printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
@@ -166,6 +241,12 @@ int main(int argc, char *argv[])
             /* -ve Test. it will fail because it expects type Int32, not String*/
             printf("calling rbus set for [%s] with string value.  this should fail.\n", paramNames[count]);
             rbusValue_SetString(value, "12234");
+            rc = rbus_set(handle, paramNames[count], value, &opts);
+            if (rc != RBUS_ERROR_SUCCESS)
+                printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
+            /* -ve Test with invalid data type. it will fail because it expects type Uint32*/
+            printf("calling rbus set for [%s] with invalid data type.  this should fail.\n", paramNames[count]);
+            rbusValue_SetFromString(value, RBUS_STRING, "1500");
             rc = rbus_set(handle, paramNames[count], value, &opts);
             if (rc != RBUS_ERROR_SUCCESS)
                 printf ("rbus_set Failed for [%s] with error [%d]\n", paramNames[count], rc);
@@ -198,7 +279,7 @@ int main(int argc, char *argv[])
     {
         printf ("Failed to discover element array. Error Code = %s\n", "");
     }
-    char* elementNames1[] = {"Device.DeviceInfo.SampleProvider.Manufacturer","Device.DeviceInfo.SampleProvider.ModelName","Device.DeviceInfo.SampleProvider.SoftwareVersion","Device.SampleProvider.SampleData.IntData","Device.SampleProvider.SampleData.BoolData","Device.SampleProvider.SampleData.UIntData","Device.SampleProvider.NestedObject1.TestParam","Device.SampleProvider.NestedObject1.AnotherTestParam","Device.SampleProvider.NestedObject2.TestParam","Device.SampleProvider.NestedObject2.AnotherTestParam"};
+    char const* elementNames1[] = {"Device.DeviceInfo.SampleProvider.Manufacturer","Device.DeviceInfo.SampleProvider.ModelName","Device.DeviceInfo.SampleProvider.SoftwareVersion","Device.SampleProvider.SampleData.IntData","Device.SampleProvider.SampleData.BoolData","Device.SampleProvider.SampleData.UIntData","Device.SampleProvider.NestedObject1.TestParam","Device.SampleProvider.NestedObject1.AnotherTestParam","Device.SampleProvider.NestedObject2.TestParam","Device.SampleProvider.NestedObject2.AnotherTestParam"};
     int numComponents = 0;
     char **componentName = NULL;
     rc = rbus_discoverComponentName(handle,10,elementNames1,&numComponents,&componentName);
@@ -252,9 +333,25 @@ int main(int argc, char *argv[])
             usleep (250000);
         }
     }
+    if(argc == 4)
+    {
+        rbusValueType_t type = RBUS_NONE;
+        rbusValue_t value;
+        rbusSetOptions_t opts;
+        opts.commit = true;
+        opts.sessionId = 0;
+        rbusValue_Init(&value);
+        type = getDataType_fromString(argv[2]);
+        rbusValue_SetFromString(value, type, argv[3]);
+        rc = rbus_set(handle, argv[1], value, &opts);
+        if (rc != RBUS_ERROR_SUCCESS)
+            printf ("rbus_set Failed for [%s] with error [%d]\n", argv[1], rc);
+        else
+            printf ("Excecution Succeeded\n");
+    }
     rbus_close(handle);
 exit1:
-    printf("constumer: exit\n");
+    printf("consumer: exit\n");
     return 0;
 }
 
